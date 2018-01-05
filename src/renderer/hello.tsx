968f77d3fd33150b65ad843edb646ef0ba02d84c
WMXPY <wm@mengw.io>
2018-01-05 14:18:48 -0600

    init

--- src/renderer/hello.tsx
+++ src/renderer/hello.tsx
@@ -3,13 +3,11 @@ import TTest from "./test";
 
 export interface IHelloProps { compiler: string; framework: string; }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export class Hello extends React.Component<IHelloProps, {}> {
     public render() {
         return <div><h1>
             Hello from {this.props.compiler} and {this.props.framework}!!!! is that magic strinsg! isit??
-            </h1>12311
+            </h1>12311afsafsafa
             <TTest></TTest>
         </div>;
     }
--- src/renderer/test.tsx
+++ src/renderer/test.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export default class TEST extends React.Component {
     public render() {
-        return <h1>1231111</h1>;
+        return <h1>sadas</h1>;
     }
 }

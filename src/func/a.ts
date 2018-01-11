fe387061111577ef380f2c2c267d0eec6d9b9204
WMXPY <wm@mengw.io>
2018-01-10 23:00:03 -0600

    use composer to generage message

--- src/func/a.ts
+++ src/func/a.ts
@@ -1,23 +1,36 @@
 import * as Imap from 'imap';
-const b = new Imap({
-    user: 'eilloytest@mail.com',
-    password: 'R2pOD2E6sYttC0h',
-    host: 'imap.mail.com',
-    port: 993,
-    tls: true,
-    tlsOptions: {
-        rejectUnauthorized: false,
-    },
-});
-b.on('ready', () => {
-    // b.copy()
-    b.append('test', {
-        mailbox: 'test',
-    }, (err) => {
-        console.log(err, 'test');
-        b.end();
-    });
+import c from 'nodemailer/lib/mail-composer';
 
-});
-b.connect();
+// const b = new Imap({
+//     user: 'eilloytest@mail.com',
+//     password: 'R2pOD2E6sYttC0h',
+//     host: 'imap.mail.com',
+//     port: 993,
+//     tls: true,
+//     tlsOptions: {
+//         rejectUnauthorized: false,
+//     },
+// });
+// b.on('ready', () => {
+//     // b.copy()
+//     b.append('test', {
+//         mailbox: 'test',
+//     }, (err) => {
+//         console.log(err, 'test');
+//         b.end();
+//     });
 
+// });
+// b.connect();
+declare const require: any;
+const MailComposer = require('nodemailer/lib/mail-composer');
+
+let a = new MailComposer({
+    from: 't@t.com',
+    to: 't',
+});
+
+a.compile().build((err: any, message: any) => {
+    console.log(message);
+    process.stdout.write(message);
+});
